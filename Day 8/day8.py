6d3e7fba9dffe042e770b113dcb2ace087bc31b0
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2019-12-21 23:06:11 -0300

    Day 8 part 1 completed

---
+++ Day 8/day8.py
@@ -0,0 +1,68 @@
+# reading the input
+data = open('input.txt', 'r').read()
+
+imageWidth = 25
+imageHeight = 6
+digitsPerLayer = imageWidth * imageHeight
+
+cutStartingPoint = 0
+cutEndingPoint = digitsPerLayer
+
+# populating the layers
+layers = []
+
+while cutEndingPoint <= len(data):
+	currentRow = []
+	currentLayerData = data[cutStartingPoint:cutEndingPoint]
+
+	lineStartingPoint = 0
+	lineEndingPoint = imageWidth
+
+	for i in range(0, imageHeight):
+		currentLine = currentLayerData[lineStartingPoint:lineEndingPoint]
+		currentRow.append(currentLine)
+		lineStartingPoint += imageWidth
+		lineEndingPoint += imageWidth
+
+	layers.append(currentRow)
+
+	cutStartingPoint += digitsPerLayer
+	cutEndingPoint += digitsPerLayer
+
+# searching for the layer with fewer 0s
+layerWithFewer0s = 0
+zerosInTheLayerWithFewerZeroes = float("inf")
+
+for layer in range(0, len(layers)):
+	zerosInThisLayer = 0
+	for row in range(0,imageHeight):
+		for line in range(0,imageWidth):
+			if layers[layer][row][line] == '0':
+				zerosInThisLayer += 1
+
+	if zerosInThisLayer < zerosInTheLayerWithFewerZeroes:
+		layerWithFewer0s = layer
+		zerosInTheLayerWithFewerZeroes = zerosInThisLayer
+
+
+# counting the number of 1s and 2s and multiplytin them
+numberOf1s = 0
+numberOf2s = 0
+
+for row in layers[layerWithFewer0s]:
+	for digit in row:
+		if digit == '1':
+			numberOf1s += 1
+		elif digit == '2':
+			numberOf2s += 1
+
+print(numberOf1s * numberOf2s)
+
+
+
+
+
+
+
+
+
